94ca8d438db907d8398f71a515d68ae462ad5cc0
Tirea Aean <tirea@learnnavi.org>
2017-12-29 10:54:59 -0500

    housecleaning

--- fwew.py
+++ fwew.py
@@ -5,41 +5,41 @@ import asyncio
 import subprocess
 
 client = discord.Client()
-
+token = "placeDiscordTokenHere"
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print("------)
 
 
 @client.event
 async def on_message(message):
     # seen the trigger word. also don't allow interactive mode
-    if message.content.startswith('!fwew') and message.content != "!fwew":
+    if message.content.startswith("!fwew") and message.content != "!fwew":
         # "fwew"
         prog = message.content[1:6]
         # remove all the sketchy chars from arguments
         nospec = message.content[6:]
         for c in "`~@#$%^&*()[]{}<>_/,;:!?|+\"\\":
-            nospec = nospec.replace(c, '')
+            nospec = nospec.replace(c, "")
         argv = nospec.split()
         # build argument string putting quotes only where necessary
         argstr = ""
         for arg in argv:
-            if arg.startswith('-'):
-                argstr += arg + ' '
+            if arg.startswith("-"):
+                argstr += arg + " "
             else:
-                argstr += '"' + arg + '"' + ' '
+                argstr += '"' + arg + '"' + " "
         # don't try to look up just a quote character
         if argstr == "" or argstr == "'":
             pass
         else:
-            response = '```'
-            response += subprocess.getoutput(prog + '-i -ipa ' + argstr)
-            response += '```'
+            response = "```"
+            response += subprocess.getoutput(prog + "-i -ipa " + argstr)
+            response += "```"
             await client.send_message(message.channel, response)
 
-client.run('token')
+client.run(token)
