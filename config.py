2f8a16feca125fcbf7bc50cda70fd107d0f49f77
Tirea Aean <tirea@learnnavi.org>
2020-01-30 19:13:02 -0500

    Update TunaYayo PFP URL

--- config.py
+++ config.py
@@ -15,6 +15,6 @@ cfg = {
     "bad_chars": "`~@#$%^&(){}<>_;:!?|\\",
     "eywa_url": "https://cdn.discordapp.com/attachments/154318499722952704/401596598624321536/image.png",
     "hrh_url": "https://youtu.be/-AgnLH7Dw3w?t=4m14s",
-    "tuna_url": "https://cdn.discordapp.com/avatars/277818358655877125/42371a0df717f9d079ba1ff7beaa8a93.png?",
+    "tuna_url": "https://cdn.discordapp.com/avatars/277818358655877125/42f3dfc47d25437edeb18729d07b7200.png",
     "token": "placeDiscordBotTokenHere",
 }
